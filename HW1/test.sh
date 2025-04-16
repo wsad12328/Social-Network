d14483d16405e00f24f6fb32c60a9cfa5b77f591
wsad1232885 <wsad1232885@gmail.com>
2025-04-16 18:49:37 +0800

    delete some uneeded file

--- HW1/test.sh
+++ HW1/test.sh
@@ -9,14 +9,6 @@ fi
 MIN=$1
 MAX=$2
 
-<<<<<<< HEAD
-epoch=(50 100 150 200)
-
-for i in "${epoch[@]}"
-do
-  python src/test.py --epochs $i --test_graphs_min $MIN --test_graphs_max $MAX 
-done
-=======
 # python src/test.py --num_min 100 --num_max 200
 # python src/test.py --num_min 200 --num_max 300
 # python src/test.py --num_min 1000 --num_max 1200
@@ -26,4 +18,3 @@ done
 # python src/test.py --num_min 4000 --num_max 5000 --test_graphs_min $MIN --test_graphs_max $MAX --test_data_path ../DrBC_Data/Synthetic
 
 python src/test.py --num_min 4000 --num_max 5000 --test_data_path ../DrBC_Data/Real/ --test_graphs_min $MIN --test_graphs_max $MAX
->>>>>>> 0c6b445 (fix Generate Testing Data & still try to learn a good model on both com-youtube and synthetic)
